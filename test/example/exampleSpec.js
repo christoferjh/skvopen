516caaf99765d145f52086e5e47ac36ff14c94ba
Johan Cronsioe <johan.cronsioe@gmail.com>
2016-09-07 20:50:52 +0200

    lade till exempel på test

---
+++ test/example/exampleSpec.js
@@ -0,0 +1,11 @@
+
+describe('ett exempel', function() {
+
+    it('testar något', function() {
+        expect(1).toBe(1);
+    });
+
+    it('testar en annan grej', function() {
+        expect(true).toBe(true);
+    });
+});
