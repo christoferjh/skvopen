a5a2383a5be53aa38de278031d41e66c82076ae0
Johan Cronsioe <johan.cronsioe@gmail.com>
2016-09-11 21:34:41 +0200

    använder angular 1 istället

--- frontend/main.js
+++
@@ -1,7 +0,0 @@
-(function(app) {
-    document.addEventListener('DOMContentLoaded', function() {
-        ng.platformBrowserDynamic
-        .platformBrowserDynamic()
-        .bootstrapModule(app.SkvopenModule);
-    });
-})(window.app || (window.app = {}));
