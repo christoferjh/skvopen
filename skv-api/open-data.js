8af0874e3fef53e3f0828dd19749f63566f1a6b8
jb <jb@jb-Aspire-5750G.(none)>
2016-09-13 21:37:39 +0200

    försök till db-api rofl....

---
+++ skv-api/open-data.js
@@ -0,0 +1,27 @@
+
+module.exports = {
+
+    lol: function() {
+        return "hej";
+    },
+
+
+    connectToDataBase: function(mongoclient, mongoURI) {
+        mongoclient.connect(mongoURI, function(err, db) {
+            if(err) { return console.log(err);}
+            
+            console.log("Connected to DB!");
+            //return mongoclient;
+            return db;   
+        })
+    }
+
+    
+    getKommunalSkattForKommun : function(mongoclient,kommunNamn) {
+        var kommunalskatt_collection = mongoclient.collection('kommunalskatt');
+        kommunalskatt_collection.find()
+
+    }
+
+
+}
