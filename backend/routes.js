d0f944bf4d8edf66fc4ab20c84ee9ad2571085f6
Johan Cronsioe <johan.cronsioe@gmail.com>
2016-09-16 16:35:28 +0200

    började på hjälpsida

--- backend/routes.js
+++ backend/routes.js
@@ -3,5 +3,9 @@ module.exports = function(app) {
     app.get('/', function(req, res) {
         res.sendFile('frontend/index.html', {root:'.'});
     });
+
+    app.get('/about', function(req, res) {
+        res.sendFile('frontend/about.html', {root:'.'});
+    });
     
 }
